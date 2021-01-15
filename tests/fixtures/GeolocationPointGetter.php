8cc48308b8fc5fd413344c77008763600c2d3a25
Petr Parolek <petr.parolek@gmail.com>
2021-01-15 21:44:30 +0100

    PHPUnit -> Nette Tester

--- tests/fixtures/GeolocationPointGetter.php
+++ tests/fixtures/GeolocationPointGetter.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Tests\Nettrine\Extensions\KnpLabs\Fixtures;
+namespace Tests\Fixtures;
 
 use Knp\DoctrineBehaviors\ORM\Geocodable\Type\Point;
 
--- tests/fixtures/LocaleGetter.php
+++ tests/fixtures/LocaleGetter.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Tests\Nettrine\Extensions\KnpLabs\Fixtures;
+namespace Tests\Fixtures;
 
 final class LocaleGetter
 {
