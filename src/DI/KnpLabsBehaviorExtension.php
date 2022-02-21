dc6197ccd3b9609f64241ba019aa870e3985e4a1
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2022-02-21 14:37:31 +0100

    knplabs v2

--- src/DI/KnpLabsBehaviorExtension.php
+++ src/DI/KnpLabsBehaviorExtension.php
@@ -3,31 +3,28 @@
 namespace Nettrine\Extensions\KnpLabs\DI;
 
 use Contributte\DI\Helper\ExtensionDefinitionsHelper;
-use Doctrine\ORM\Mapping\ClassMetadataInfo;
-use Knp\DoctrineBehaviors\Model\Blameable\Blameable;
-use Knp\DoctrineBehaviors\Model\Geocodable\Geocodable;
-use Knp\DoctrineBehaviors\Model\Sluggable\Sluggable;
-use Knp\DoctrineBehaviors\Model\SoftDeletable\SoftDeletable;
-use Knp\DoctrineBehaviors\Model\Sortable\Sortable;
-use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
-use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
-use Knp\DoctrineBehaviors\Model\Translatable\Translation;
-use Knp\DoctrineBehaviors\Model\Tree\Node;
-use Knp\DoctrineBehaviors\ORM\Blameable\BlameableSubscriber;
-use Knp\DoctrineBehaviors\ORM\Geocodable\GeocodableSubscriber;
-use Knp\DoctrineBehaviors\ORM\Loggable\LoggableSubscriber;
-use Knp\DoctrineBehaviors\ORM\Sluggable\SluggableSubscriber;
-use Knp\DoctrineBehaviors\ORM\SoftDeletable\SoftDeletableSubscriber;
-use Knp\DoctrineBehaviors\ORM\Sortable\SortableSubscriber;
-use Knp\DoctrineBehaviors\ORM\Timestampable\TimestampableSubscriber;
-use Knp\DoctrineBehaviors\ORM\Translatable\TranslatableSubscriber;
-use Knp\DoctrineBehaviors\ORM\Tree\TreeSubscriber;
-use Knp\DoctrineBehaviors\Reflection\ClassAnalyzer;
+use Knp\DoctrineBehaviors\Model\Blameable\BlameableTrait;
+use Knp\DoctrineBehaviors\Model\Sluggable\SluggableTrait;
+use Knp\DoctrineBehaviors\Model\SoftDeletable\SoftDeletableTrait;
+use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslationTrait;
+use Knp\DoctrineBehaviors\Model\Tree\TreeNodeTrait;
+use Knp\DoctrineBehaviors\EventSubscriber\BlameableEventSubscriber;
+use Knp\DoctrineBehaviors\EventSubscriber\LoggableEventSubscriber;
+use Knp\DoctrineBehaviors\EventSubscriber\SluggableEventSubscriber;
+use Knp\DoctrineBehaviors\EventSubscriber\SoftDeletableEventSubscriber;
+use Knp\DoctrineBehaviors\EventSubscriber\TimestampableEventSubscriber;
+use Knp\DoctrineBehaviors\EventSubscriber\TranslatableEventSubscriber;
+use Knp\DoctrineBehaviors\EventSubscriber\TreeEventSubscriber;
+use Knp\DoctrineBehaviors\Repository\DefaultSluggableRepository;
 use Nette\DI\CompilerExtension;
-use Nette\DI\Statement;
+use Nette\DI\Definitions\Statement;
 use Nette\Schema\Elements\AnyOf;
 use Nette\Schema\Expect;
 use Nette\Schema\Schema;
+use Nettrine\Extensions\KnpLabs\Security\UserCallable;
+use Nettrine\Extensions\KnpLabs\Translatable\DefaultLocaleProvider;
 use stdClass;
 
 /**
@@ -55,64 +52,50 @@ final class KnpLabsBehaviorExtension extends CompilerExtension
 			'blameable' => Expect::anyOf(
 				Expect::bool(),
 				Expect::structure([
-					'trait' => Expect::string(Blameable::class),
-					'userCallable' => $this->getCallableSchema()->default(null),
+					'trait' => Expect::string(BlameableTrait::class),
+					'userCallable' => $this->getCallableSchema()->default(UserCallable::class),
 					'userEntity' => Expect::string(null),
-				])
-			)->default(false)->before($trueToStructureCb),
-			'geocodable' => Expect::anyOf(
-				Expect::bool(),
-				Expect::structure([
-					'trait' => Expect::string(Geocodable::class),
-					'geolocationCallable' => $this->getCallableSchema()->default(null),
 				])
 			)->default(false)->before($trueToStructureCb),
 			'loggable' => Expect::anyOf(
 				false,
 				Expect::structure([
-					'loggerCallable' => $this->getCallableSchema()->required(),
+					'logger' => $this->getCallableSchema()->required(),
 				])
 			)->default(false),
 			'sluggable' => Expect::anyOf(
 				Expect::bool(),
 				Expect::structure([
-					'trait' => Expect::string(Sluggable::class),
+					'trait' => Expect::string(SluggableTrait::class),
 				])
 			)->default(false)->before($trueToStructureCb),
 			'softDeletable' => Expect::anyOf(
 				Expect::bool(),
 				Expect::structure([
-					'trait' => Expect::string(SoftDeletable::class),
-				])
-			)->default(false)->before($trueToStructureCb),
-			'sortable' => Expect::anyOf(
-				Expect::bool(),
-				Expect::structure([
-					'trait' => Expect::string(Sortable::class),
+					'trait' => Expect::string(SoftDeletableTrait::class),
 				])
 			)->default(false)->before($trueToStructureCb),
 			'timestampable' => Expect::anyOf(
 				Expect::bool(),
 				Expect::structure([
-					'trait' => Expect::string(Timestampable::class),
+					'trait' => Expect::string(TimestampableTrait::class),
 					'dbFieldType' => Expect::string('datetime'),
 				])
 			)->default(false)->before($trueToStructureCb),
 			'translatable' => Expect::anyOf(
 				Expect::bool(),
 				Expect::structure([
-					'currentLocaleCallable' => $this->getCallableSchema()->default(null),
-					'defaultLocaleCallable' => $this->getCallableSchema()->default(null),
-					'translatableFetchMode' => Expect::anyOf('LAZY', 'EAGER', 'EXTRA_LAZY', Expect::int())->default(ClassMetadataInfo::FETCH_LAZY),
-					'translationFetchMode' => Expect::anyOf('LAZY', 'EAGER', 'EXTRA_LAZY', Expect::int())->default(ClassMetadataInfo::FETCH_LAZY),
-					'translatableTrait' => Expect::string(Translatable::class),
-					'translationTrait' => Expect::string(Translation::class),
+					'localeProvider' => $this->getCallableSchema()->default(DefaultLocaleProvider::class),
+					'translatableFetchMode' => Expect::anyOf('LAZY', 'EAGER', 'EXTRA_LAZY', Expect::int())->default('LAZY'),
+					'translationFetchMode' => Expect::anyOf('LAZY', 'EAGER', 'EXTRA_LAZY', Expect::int())->default('LAZY'),
+					'translatableTrait' => Expect::string(TranslatableTrait::class),
+					'translationTrait' => Expect::string(TranslationTrait::class),
 				])
 			)->default(false)->before($trueToStructureCb),
 			'tree' => Expect::anyOf(
 				Expect::bool(),
 				Expect::structure([
-					'nodeTrait' => Expect::string(Node::class),
+					'nodeTrait' => Expect::string(TreeNodeTrait::class),
 				])
 			)->default(false)->before($trueToStructureCb),
 		]);
@@ -124,108 +107,65 @@ final class KnpLabsBehaviorExtension extends CompilerExtension
 		$config = $this->config;
 		$definitionsHelper = new ExtensionDefinitionsHelper($this->compiler);
 
-		$classAnalyzerDefinition = $builder->addDefinition($this->prefix('classAnalyzer'))
-			->setFactory(ClassAnalyzer::class)
-			->setAutowired(false);
-		$isRecursive = true;
-
 		if ($config->blameable !== false) {
 			$builder->addDefinition($this->prefix('blameable'))
-				->setFactory(BlameableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$isRecursive,
-					$config->blameable->trait,
-					$config->blameable->userCallable === null ? null : $definitionsHelper->getCallableFromConfig(
+				->setType(BlameableEventSubscriber::class)
+				->setArguments([
+					'userProvider' => $definitionsHelper->getCallableFromConfig(
 						$config->blameable->userCallable,
 						$this->prefix('blameable.callback')
 					),
-					$config->blameable->userEntity,
-				])
-				->setAutowired(false);
-		}
-
-		if ($config->geocodable !== false) {
-			$builder->addDefinition($this->prefix('geocodable'))
-				->setFactory(GeocodableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$isRecursive,
-					$config->geocodable->trait,
-					$config->geocodable->geolocationCallable === null ? null : $definitionsHelper->getCallableFromConfig(
-						$config->geocodable->geolocationCallable,
-						$this->prefix('geocodable.callback')
-					),
+					'blameableUserEntity' => $config->blameable->userEntity,
 				])
 				->setAutowired(false);
 		}
 
 		if ($config->loggable !== false) {
 			$builder->addDefinition($this->prefix('loggable'))
-				->setFactory(LoggableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$isRecursive,
-					$definitionsHelper->getCallableFromConfig(
-						$config->loggable->loggerCallable,
-						$this->prefix('loggable.callback')
-					),
+				->setType(LoggableEventSubscriber::class)
+				->setArguments([
+					$config->loggable->logger,
 				])
 				->setAutowired(false);
 		}
 
 		if ($config->sluggable !== false) {
+			$sluggableRepository = $builder->addDefinition($this->prefix('suggable.repository'))
+				->setType(DefaultSluggableRepository::class)
+				->setAutowired(false);
+
 			$builder->addDefinition($this->prefix('sluggable'))
-				->setFactory(SluggableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$isRecursive,
-					$config->sluggable->trait,
+				->setType(SluggableEventSubscriber::class)
+				->setArguments([
+					'defaultSluggableRepository' => $sluggableRepository
 				])
 				->setAutowired(false);
 		}
 
 		if ($config->softDeletable !== false) {
 			$builder->addDefinition($this->prefix('softDeletable'))
-				->setFactory(SoftDeletableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$isRecursive,
-					$config->softDeletable->trait,
-				])
-				->setAutowired(false);
-		}
-
-		if ($config->sortable !== false) {
-			$builder->addDefinition($this->prefix('sortable'))
-				->setFactory(SortableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$isRecursive,
-					$config->sortable->trait,
-				])
+				->setType(SoftDeletableEventSubscriber::class)
 				->setAutowired(false);
 		}
 
 		if ($config->timestampable !== false) {
 			$builder->addDefinition($this->prefix('timestampable'))
-				->setFactory(TimestampableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$isRecursive,
-					$config->timestampable->trait,
+				->setType(TimestampableEventSubscriber::class)
+				->setArguments([
 					$config->timestampable->dbFieldType,
 				])
 				->setAutowired(false);
 		}
 
 		if ($config->translatable !== false) {
+			$localeProvider = $builder->addDefinition($this->prefix('translatable.localeProvider'))
+				->setType($config->translatable->localeProvider)
+				->setAutowired(false);
+
 			$builder->addDefinition($this->prefix('translatable'))
-				->setFactory(TranslatableSubscriber::class, [
-					$classAnalyzerDefinition,
-					$config->translatable->currentLocaleCallable === null ? null : $definitionsHelper->getCallableFromConfig(
-						$config->translatable->currentLocaleCallable,
-						$this->prefix('translatable.currentLocaleCallback')
-					),
-					$config->translatable->defaultLocaleCallable === null ? null : $definitionsHelper->getCallableFromConfig(
-						$config->translatable->defaultLocaleCallable,
-						$this->prefix('translatable.defaultLocaleCallback')
-					),
-					$config->translatable->translatableTrait,
-					$config->translatable->translationTrait,
+				->setType(TranslatableEventSubscriber::class)
+				->setArguments([
+					$localeProvider,
 					$config->translatable->translatableFetchMode,
 					$config->translatable->translationFetchMode,
 				])
@@ -237,11 +177,7 @@ final class KnpLabsBehaviorExtension extends CompilerExtension
 		}
 
 		$builder->addDefinition($this->prefix('tree'))
-			->setFactory(TreeSubscriber::class, [
-				$classAnalyzerDefinition,
-				$isRecursive,
-				$config->tree->nodeTrait,
-			])
+			->setType(TreeEventSubscriber::class)
 			->setAutowired(false);
 	}
 
