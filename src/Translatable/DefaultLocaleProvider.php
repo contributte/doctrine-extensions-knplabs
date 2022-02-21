80bb797b7f4f482c93f2135329aed345d43f9fea
Pavel Jurásek <jurasekpavel@ctyrimedia.cz>
2022-02-21 14:37:31 +0100

    knplabs v2

---
+++ src/Translatable/DefaultLocaleProvider.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace Nettrine\Extensions\KnpLabs\Translatable;
+
+use Knp\DoctrineBehaviors\Contract\Provider\LocaleProviderInterface;
+
+class DefaultLocaleProvider implements LocaleProviderInterface
+{
+
+	public function provideCurrentLocale(): ?string
+	{
+		return null;
+	}
+
+
+	public function provideFallbackLocale(): ?string
+	{
+		return null;
+	}
+
+}
